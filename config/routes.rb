f25bfba70fea45f1ea08a7c0c555eb19db839830
Amohamed96 <amohamed.rg@gmail.com>
2022-01-11 13:45:33 -0500

    Admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
+
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
